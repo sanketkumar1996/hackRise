caba3c481d53d7afd1bb54df40c1c54f9bac7a11
sanketkumar1996 <sanketkumar1996@users.noreply.github.com>
2016-09-24 16:16:59 +0530

    Update getuserAlldetails.php

--- getuserAlldetails.php
+++ getuserAlldetails.php
@@ -3,18 +3,18 @@ if(isset($_GET['user'])){
 $userid = $_GET['user'];
 $bank = $_GET['bank'];
 if ( $userid == "user1" && $bank=="barclays") {
-	echo file_get_contents("C:\Users\DynamiteChetan\Desktop\apis\user1.json");
+	echo file_get_contents("user1.json");
 }
 else if($userid == "user2"&& $bank=="barclays"){	
-	echo file_get_contents("C:\Users\DynamiteChetan\Desktop\apis\user2.json");
+	echo file_get_contents("user2.json");
 }
 
 
 else if($userid == "user1" && $bank=="HDFC"){
-	echo file_get_contents("C:\Users\DynamiteChetan\Desktop\apis\user1_hdfc.json");
+	echo file_get_contents("user1_hdfc.json");
 }
 else if($userid == "user2"&& $bank=="HDFC"){	
-	echo file_get_contents("C:\Users\DynamiteChetan\Desktop\apis\user2_hdfc.json");
+	echo file_get_contents("user2_hdfc.json");
 }
 
 
